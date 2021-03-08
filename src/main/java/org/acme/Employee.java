adfc978872dab7d0fbd6dfddf87c2551b8c9d7aa
MajauskasS <80051186+MajauskasS@users.noreply.github.com>
2021-03-08 11:23:07 +0200

    Update Employee.java
    Added comments for the parts of the class.

--- src/main/java/org/acme/Employee.java
+++ src/main/java/org/acme/Employee.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Employee {
-    String fullName;
-    String team;
-    String performanceRating;
-    boolean isIntern;
-    int yearlySalary;
-    int employmentTime;
-    List<Integer> yearlyBonuses = new ArrayList<Integer>();
+    String fullName; //Employee's full name
+    String team; //What team the employee belongs to - Senior Management receives double bonus.
+    String performanceRating; //What the employee's performance review was - Exceptional performance increases the bonus by 50%
+    boolean isIntern; //Whether the employee is an intern - interns get no bonuses.
+    int yearlySalary; //What the employee's current yearly salary is - 5 year bonuses cannot exceed current yearly salary x6
+    int employmentTime; //How long the employee has worked in the company - time defines the percentage of yearly salary for the bonus
+    List<Integer> yearlyBonuses = new ArrayList<Integer>(); //The list of previous bonuses that are relevant to the bonus calculation.
 }

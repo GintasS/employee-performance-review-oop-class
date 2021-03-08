66fb5a6c80cf331e541bf3eadeb4bfc02944e6bf
MajauskasS <80051186+MajauskasS@users.noreply.github.com>
2021-03-08 11:19:03 +0200

    Create Employee.java
    A full class for the Employees, including every aspect that would affect the yearly bonus.

---
+++ src/main/java/org/acme/Employee.java
@@ -0,0 +1,14 @@
+package org.acme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    String fullName;
+    String team;
+    String performanceRating;
+    boolean isIntern;
+    int yearlySalary;
+    int employmentTime;
+    List<Integer> yearlyBonuses = new ArrayList<Integer>();
+}

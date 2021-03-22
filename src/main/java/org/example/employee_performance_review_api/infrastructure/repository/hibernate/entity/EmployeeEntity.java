ad1baa70c84136e0bdbddd36caa21d8c6089588e
GintasS <svedgintas@gmail.com>
2021-03-23 01:34:57 +0200

    Refactored our entity class that directly deals with file IO.

--- src/main/java/org/example/employee_performance_review_api/infrastructure/repository/hibernate/entity/EmployeeEntity.java
+++ src/main/java/org/example/employee_performance_review_api/infrastructure/repository/hibernate/entity/EmployeeEntity.java
@@ -3,35 +3,65 @@ package org.example.employee_performance_review_api.infrastructure.repository.hi
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.example.employee_performance_review_api.domain.model.constants.ValidationMessages;
 import org.example.employee_performance_review_api.domain.model.employee.Employee;
+import org.example.employee_performance_review_api.domain.model.employee.enums.EmployeeType;
+import org.example.employee_performance_review_api.domain.model.employee.enums.PerformanceRating;
+import org.example.employee_performance_review_api.domain.model.employee.enums.Team;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "EMPLOYEES")
 public class EmployeeEntity {
 
-  @Id private UUID id;
+  // This is a unique identifier for every employee.
+  private String id;
 
   private String firstName;
   private String lastName;
-
+  private Team team;
+  private PerformanceRating rating;
+  private EmployeeType type;
+  private int yearlySalary;
+  private int employmentTime;
+  private List<Integer> yearlyBonuses;
 
   public EmployeeEntity(Employee employee) {
-    this.id  = employee.getId();
     update(employee);
   }
 
+  public EmployeeEntity(UUID id, String firstName, String lastName, Team team, PerformanceRating rating, EmployeeType type,
+                        int yearlySalary, int employmentTime, List<Integer> yearlyBonuses)
+  {
+    this.id = id.toString();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.team = team;
+    this.rating = rating;
+    this.type = type;
+    this.yearlySalary = yearlySalary;
+    this.employmentTime = employmentTime;
+    this.yearlyBonuses = yearlyBonuses;
+  }
+
+
   public void update(Employee employee) {
+    this.id = employee.getId().toString();
     this.firstName = employee.getFirstName();
     this.lastName = employee.getLastName();
-
-    // TODO: finish initializing
+    this.team = employee.getTeam();
+    this.rating = employee.getRating();
+    this.type = employee.getType();
+    this.yearlySalary = employee.getYearlySalary();
+    this.employmentTime = employee.getEmploymentTime();
+    this.yearlyBonuses = employee.getYearlyBonuses();
   }
 }

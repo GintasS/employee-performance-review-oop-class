bb829eb0aac2f8aeee6f00deefbeda03da650e2f
GintasS <svedgintas@gmail.com>
2021-03-23 01:33:29 +0200

    Added helper classes to deal with json, file IO and lists.

---
+++ src/main/java/org/example/employee_performance_review_api/infrastructure/repository/hibernate/helpers/FileHelper.java
@@ -0,0 +1,30 @@
+package org.example.employee_performance_review_api.infrastructure.repository.hibernate.helpers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHelper {
+    public static String ReadFile(String filePath) {
+        String content = "";
+
+        try {
+            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return content;
+    }
+
+    public static void WriteToFile(String filePath, String data) {
+        try {
+            Files.writeString(Path.of(filePath), data);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}

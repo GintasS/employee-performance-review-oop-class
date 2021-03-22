464f11af0dd73f6657aff287e77dc8a1fa1afc33
GintasS <svedgintas@gmail.com>
2021-03-23 01:28:34 +0200

    Updated helper dto classes with all the necessary fields.

--- src/main/java/org/example/employee_performance_review_api/domain/model/employee/Employee.java
+++ src/main/java/org/example/employee_performance_review_api/domain/model/employee/Employee.java
@@ -4,27 +4,47 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.example.employee_performance_review_api.domain.model.constants.ValidationMessages;
+import org.example.employee_performance_review_api.domain.model.employee.enums.EmployeeType;
+import org.example.employee_performance_review_api.domain.model.employee.enums.PerformanceRating;
+import org.example.employee_performance_review_api.domain.model.employee.enums.Team;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Employee {
 
+    // This is a unique identifier for every employee.
     @NotNull
-    private UUID id; //REST endpoint
-    @NotBlank(message = ValidationMessages.USERNAME_MUST_BE_NOT_BLANK)
-    private String firstName; //Employee's first name
-    @NotBlank(message = ValidationMessages.EMAIL_MUST_BE_NOT_BLANK)
-    private String lastName; //Employee's last name
-    //private Team team; //What team the employee belongs to - Senior Management receives double bonus.
-    //private PerformanceRating rating;//What the employee's performance review was - Exceptional performance increases the bonus by 50%
-    //private EmployeeType type; //Interns get no bonuses.
-    //private int yearlySalary; //What the employee's current yearly salary is - 5 year bonuses cannot exceed current yearly salary x6
-    //private int employmentTime; //How long (in years) the employee has worked in the company - time defines the percentage of yearly salary for the bonus
-    //private List<Integer> yearlyBonuses = new ArrayList<Integer>(); //The list of previous bonuses that are relevant to the bonus calculation.
+    private UUID id;
 
-    // TODO: make all the access to private ?
+    @NotBlank(message = ValidationMessages.EMPLOYEE_FIRST_NAME_MUST_BE_NOT_BLANK)
+    private String firstName;
+
+    @NotBlank(message = ValidationMessages.EMPLOYEE_LAST_NAME_MUST_BE_NOT_BLANK)
+    private String lastName;
+
+    // What team the employee belongs to - Senior Management receives double bonus.
+    private Team team;
+
+    // What the employee's performance review is.
+    // Exceptional performance increases the bonus by 50%.
+    private PerformanceRating rating;
+
+    // Interns get no bonuses.
+    private EmployeeType type;
+
+    // What the employee's current yearly salary is.
+    // 5 year bonuses cannot exceed current yearly salary 6 times.
+    private int yearlySalary;
+
+    // How long (in years) the employee has worked in the company.
+    // Time defines the percentage of yearly salary for the bonus.
+    private int employmentTime;
+
+    // The list of previous bonuses that are relevant to the bonus calculation.
+    private List<Integer> yearlyBonuses;
 }
--- src/main/java/org/example/employee_performance_review_api/domain/model/employee/UpdateEmployeeInput.java
+++ src/main/java/org/example/employee_performance_review_api/domain/model/employee/UpdateEmployeeInput.java
@@ -2,7 +2,11 @@ package org.example.employee_performance_review_api.domain.model.employee;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.example.employee_performance_review_api.domain.model.employee.enums.EmployeeType;
+import org.example.employee_performance_review_api.domain.model.employee.enums.PerformanceRating;
+import org.example.employee_performance_review_api.domain.model.employee.enums.Team;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -12,6 +16,12 @@ public class UpdateEmployeeInput {
     private UUID id;
     private String firstName;
     private String lastName;
+    private Team team;
+    private PerformanceRating rating;
+    private EmployeeType type;
+    private int yearlySalary;
+    private int employmentTime;
+    private List<Integer> yearlyBonuses;
 
     // TODO finish all the values
 }

43ab8dafba03ea5ae5496d58311fc575864979ef
GintasS <svedgintas@gmail.com>
2021-03-24 00:23:00 +0200

    Added employee bonus response.

---
+++ src/main/java/org/example/employee_performance_review_api/application/web/model/response/EmployeeBonusResponse.java
@@ -0,0 +1,26 @@
+package org.example.employee_performance_review_api.application.web.model.response;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.employee_performance_review_api.domain.model.employee.Employee;
+import org.example.employee_performance_review_api.domain.model.employee.enums.EmployeeType;
+import org.example.employee_performance_review_api.domain.model.employee.enums.PerformanceRating;
+import org.example.employee_performance_review_api.domain.model.employee.enums.Team;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@JsonRootName("employeeBonus")
+@RegisterForReflection
+public class EmployeeBonusResponse {
+
+    private double bonus;
+
+    public EmployeeBonusResponse(double bonus) {
+        this.bonus = bonus;
+    }
+}

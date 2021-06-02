0159fc8526484d483584f25d1c93a6ac73a71137
GintasS <svedgintas@gmail.com>
2021-06-02 22:12:08 +0300

    Applied proper code styling.

--- src/main/java/org/example/employee_performance_review_api/infrastructure/configuration/ModelValidatorConfiguration.java
+++ src/main/java/org/example/employee_performance_review_api/infrastructure/configuration/ModelValidatorConfiguration.java
@@ -1,15 +1,14 @@
 package org.example.employee_performance_review_api.infrastructure.configuration;
 
-import org.example.employee_performance_review_api.domain.validator.ModelValidator;
-
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 import javax.validation.Validator;
+import org.example.employee_performance_review_api.domain.validator.ModelValidator;
 
 public class ModelValidatorConfiguration {
-    @Produces
-    @Singleton
-    public ModelValidator modelValidator(Validator validator) {
-        return new ModelValidator(validator);
-    }
+  @Produces
+  @Singleton
+  public ModelValidator modelValidator(Validator validator) {
+    return new ModelValidator(validator);
+  }
 }
